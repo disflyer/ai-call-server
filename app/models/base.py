22c06dbd8d02177e2b87b996f84020be5d71af07
Alan Fang <disflyer123@gmail.com>
2025-05-24 13:54:36 +0800

    init

--- app/models/base.py
+++ app/models/base.py
@@ -1,8 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from app.core.config import settings
+
+# 统一指定schema
+metadata = MetaData(schema="ai-call")
 
 engine = create_engine(settings.DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base() 
+Base = declarative_base(metadata=metadata) 
